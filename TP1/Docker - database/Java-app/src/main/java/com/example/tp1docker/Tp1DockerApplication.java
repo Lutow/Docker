b883e373fe061b536b43bf1c7a0a7ff8d30b93eb
Lutow <118168487+Lutow@users.noreply.github.com>
2025-10-27 09:13:13 +0100

    Add files via upload

---
+++ TP1/Docker - database/Java-app/src/main/java/com/example/tp1docker/Tp1DockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp1docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1DockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp1DockerApplication.class, args);
+    }
+
+}
